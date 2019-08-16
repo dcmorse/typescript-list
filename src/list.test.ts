f13cea0aeb3f4e754e4fb29a905335253d71d3d4
Dave Morse <dcmorse@gmail.com>
2019-08-15 23:39:43 -0500

    wip

--- src/list.test.ts
+++ src/list.test.ts
@@ -1,13 +1,55 @@
-import { List, nil } from './list';
+import { list, nil } from './list';
 
-describe('constructor', () => {
-  test('constructor with ', () => {
-    expect(new List()).toBe(nil);
+describe('List', () => {
+  describe('constructor and toArray()', () => {
+    test('returns nil for no args', () => {
+      expect(list()).toBe(nil);
+    });
+    test('returns (56) for list(56)', () => {
+      const l = list(56);
+      expect(l.length()).toBe(1);
+      expect(l.head).toBe(56);
+      expect(l.tail).toBe(nil);
+    })
+    test('returns (5,6,7) for list(5,6,7)', () => {
+      let l = list(5,6,7);
+      expect(l.length()).toBe(3);
+      expect(l.head).toBe(5);
+      l = l.tail;
+      expect(l.head).toBe(6);
+      l = l.tail;
+      expect(l.head).toBe(7);
+      l = l.tail;
+      expect(l).toBe(nil);
+    })
+  })
+
+  describe('head', () => {
+    it('Cons works', () => {
+      let cons = list('apple');
+      expect(cons.head).toEqual('apple');
+    })
+    it('nil explodes', () => {
+      expect(() => nil.head).toThrow(RangeError)
+    });
   });
-})
 
-describe('nil', () => {
-  test("prints as 'nil'", () => {
-    expect('' + nil).toEqual('nil');
+  describe('tail', () => {
+    it('Cons works', () => {
+      let cons = list('apple');
+      expect(cons.tail).toEqual(nil);
+    })
+    it('nil explodes', () => {
+      expect(() => nil.tail).toThrow(RangeError)
+    });
+  });
+
+  describe('nil', () => {
+    it("prints as 'nil'", () => {
+      expect(nil.toString()).toEqual('nil');
+    });
+    it("length 0", () => {
+      expect(nil.length()).toEqual(0);
+    });
   })
 })
--- src/list.ts
+++ src/list.ts
@@ -1,9 +1,49 @@
-export class List {
+interface List<T> {
+  length(): number;
+  head: T;
+  tail: List<T>;
+  // prepend(): (<T>) => List<T>;
+  // toArray()
+  // map()
+  // reduce()
 }
 
+function list<T>(...args): List<T> {
+  let ll: List<T> = nil;
+  for (let i = args.length-1; i >= 0; i--)
+    ll = new Cons(args[i], ll);
+  return ll;
+}
 
-export const nil = Object.freeze({
+export { list };
+
+export class Cons<t> {
+  head: t;
+  tail: List<t>;
+
+  constructor(head: t, tail: List<t>) {
+    this.head = head;
+    this.tail = tail;
+  };
+
+  length() {
+    return 1 + this.tail.length()
+  };
+}
+
+class Nil {
   toString() {
     return 'nil';
   }
-});
+  length() {
+    return 0;
+  }
+  get head(): never {
+    throw new RangeError('Attempt to take the head of an empty list.')
+  }
+  get tail(): never {
+    throw new RangeError('Attempt to take the tail of an empty list.');
+  }
+}
+
+export const nil = new Nil()
